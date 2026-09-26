b1cfdbc7fc2ae72d89dfbe98703039da0e8b6519
chanu2005-wq <chandrachanukya924@gmail.com>
2026-09-26 12:04:35 +0530

    upated commit for backend

--- server/configs/db.js
+++ server/configs/db.js
@@ -1,8 +1,13 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const connectDB = async () => {
-  mongoose.connection.on("connected", () => console.log("Database Connected"));
-  await mongoose.connect(`${process.env.MONGODB_URI}/automate`);
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.error("MongoDB connection failed:", error.message);
+    process.exit(1);
+  }
 };
 
 export default connectDB;
