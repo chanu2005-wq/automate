0d199c3b969d91f9e8943a313240f20dfff6e388
chanu2005-wq <chandrachanukya924@gmail.com>
2026-09-26 19:50:56 +0530

    fix(email): add SMTP connection timeouts to prevent hanging connections

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -15,7 +15,10 @@ const sendEmail = async (options) => {
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS
-      }
+      },
+      connectionTimeout: 5000,
+      greetingTimeout: 5000,
+      socketTimeout: 5000
     });
 
     const message = {
