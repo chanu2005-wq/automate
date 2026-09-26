5d28e684fdb4fc69bf6af0fc365661ec1cba32b0
chanu2005-wq <chandrachanukya924@gmail.com>
2026-09-26 19:08:04 +0530

    updated commit for backend

--- server/server.js
+++ server/server.js
@@ -95,7 +95,7 @@ app.use("/api/admin", adminRoutes);
 // Global Error Handler
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => {
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
